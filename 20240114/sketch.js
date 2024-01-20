9c6e6de241cb5381090067ca0f0b4889610dc176
ocello3 <ocello3@users.noreply.github.com>
2024-01-21 07:34:19 +0900

    updated

--- 20240114/sketch.js
+++ 20240114/sketch.js
@@ -16,20 +16,30 @@ function draw() {
 	_spirals = isInit ? [...Array(10)] : dt.spirals;
 	dt.spirals = _spirals.map((_spiral, i) => {
 		const spiral = {};
+		// reset when _radius over max
+		const isReset = _spiral.angle > size * 2 / 3;
 		spiral.angle = (() => {
-			// reset when _radius over max
+			if (isReset) return 0;
+			return _spiral.angle + 1; // todo change speed from 1
 		})();
-		spiral.a = (() => {
-			// reset when angle === 0
+		spiral.a = (() => { // > 0
+			if (!isReset) return _spiral.a;
+			return random(0.1, 10); // todo find min/max
 		})();
-		spiral.b = (() => {
-			// reset when angle === 0
+		spiral.b = (() => { 
+			if (!isReset) return _spiral.a;
+			return random(-10, 10); // todo find min/max
 		})();
 		spiral.radius = (() => {
-			// determind by angle
+			// r=ae^{b\theta}
+			const ae = spiral.a * Math.E;
+			return pow(ae, spiral.b / spiral.angle);
 		})();
 		spiral.mid = (() => {
-			// reset when angle === 0
+			if (!isReset) return _spiral.mid;
+			const norm = p5.Vector.random2D(); // 0 - 1 // here change -1 - 1
+			const diff = p5.Vector.mult(norm, size * 0.3); // todo find scale
+			return 
 		})();
 		return spiral;
 	})
